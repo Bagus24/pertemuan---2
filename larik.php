bc17c4d7393c8512c861f3477197e4e17b0527bd
Bagus24 <firgiawanbagus@gmail.com>
2018-02-27 16:11:54 +0700

    Add files via upload

---
+++ larik.php
@@ -0,0 +1,9 @@
+<?php
+$data = array("gula", "teh", "kecap");
+
+echo "Saya punya ". $data[0]. ", ". $data[1]. ", dan ". $data[2]. ". <br><br>";
+
+echo $data[0]. "<br>";
+echo $data[1]. "<br>";
+echo $data[2]. "<br>";
+?>
