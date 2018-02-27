d88bfc39e234f379eac57d715467aaa3ab4e66c2
Bagus24 <firgiawanbagus@gmail.com>
2018-02-27 16:51:24 +0700

    Add files via upload

---
+++ larik-modif.php
@@ -0,0 +1,13 @@
+<?php
+$data = array (
+    "Nama" => "Bagus",
+    "Usia" => 20,
+    "Jomblo" => true
+
+);
+
+echo "Biodata Saya <br>";
+echo "Nama : ". $data["Nama"]."<br>";
+echo "Usia : ". $data["Usia"]."<br>";
+echo "Jomblo ? : ". $data["Jomblo"]."<br>";
+?>
---
+++ larik-multi.php
@@ -0,0 +1,21 @@
+<?php
+$data = array (
+    "web-prog" =>
+        array(
+            "Bagus" =>5,
+            "Firgiawan" =>7,
+            "Prakoso" =>9
+        ),
+    "kalkulus" =>
+        array(
+            "Leonel" =>6,
+            "Andress" =>7,
+            "Messi" =>8
+        )
+);
+
+echo "Nilai web-prog Bagus : ". $data["web-prog"]["Bagus"]. "<br>";
+echo "Nilai kalkulus Messi : ". $data["kalkulus"]["Messi"]. "<br>";
+
+
+?>
